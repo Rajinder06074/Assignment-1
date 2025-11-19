7b663431da70394a9c8e1d450e6bb10d8578c67c
Rajinder Kamboj <rajinder06074@gmail.com>
2025-11-19 12:18:33 +0530

    Initial Commit

---
+++ Personalized-Greeting.py
@@ -0,0 +1,17 @@
+"""
+Task 2
+Write a Python program that does the following:
+1. Take a user's first name and last name as input.
+2. Concatenates the first name and last name into a full name.
+3. Prints a personalized greeting message using the full name.
+"""
+
+# Take first name and last name
+first_name = input("Enter your first name: ")    # Enter your first name: Rajinder
+last_name = input("Enter your last name: ")      # Enter your last name: Kamboj
+
+# Print personalized greeting message
+
+print(f"Hello, {first_name} {last_name}! Welcome to Python programming.")
+
+# Hello, Rajinder Kamboj! Welcome to Python programming.
